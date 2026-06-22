0782e170b6cab8efe11332f0a7396637a0893041
Rayongcpd <rayong.coop.archive@gmail.com>
2026-06-22 15:54:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 
 const CONFIG = {
   // Google Apps Script Web App Deployment URL
-  API_BASE: "https://script.google.com/macros/s/AKfycbx6TUKnPE5N_mUsgafTHH-6-z3SaizfpbUkAGQY1fmw0deUTaY1nozIsKhnSX1JzUj8/exec",
+  API_BASE: "https://script.google.com/macros/s/AKfycbwLXJQ5AAKaxqzG-oNAacchVqxFFjspvu5po25spYg1VOxNSletJHo-g8j3xmfBFkLC/exec",
 
   // Content Security Policy
   CSP: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:; connect-src https://script.google.com; frame-src 'none'; object-src 'none';",
